eb411ca459b8910beccab990a1b3a95d55bca0be
Jason Allen <jason@vynyl.com>
2016-07-19 11:26:59 -0600

    DATA-296 added resource for creating and updating custom fields in Campaigner library

---
+++ src/Resources/Database.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Vynyl\Campaigner\Resources;
+
+
+use Vynyl\Campaigner\DTO\DatabaseColumn;
+use Vynyl\Campaigner\Connection;
+
+class Database
+{
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function get()
+    {
+        $columns = $this->connection->get('/Database');
+        return $columns['DatabaseColumns'];
+    }
+
+    public function post(DatabaseColumn $databaseColumn)
+    {
+        $payload = $databaseColumn->toPost();
+
+        return $this->connection->post(
+            '/Database',
+            $payload
+        );
+    }
+
+}
